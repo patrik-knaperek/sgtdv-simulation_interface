852ff318ac519b44fb9d451f85b028dde6a3dc90
Patrik Knaperek <pknaperek@gmail.com>
2024-02-10 18:20:17 +0100

    Code optimization
    - corrected object allocation heap -> stack

--- path_tracking_sim_interface/src/path_tracking_sim_interface.cpp
+++ path_tracking_sim_interface/src/path_tracking_sim_interface.cpp
@@ -21,49 +21,47 @@ PathTrackingSimInterface::PathTrackingSimInterface(ros::NodeHandle& nh) :
 
 void PathTrackingSimInterface::cmdCallback(const sgtdv_msgs::Control::ConstPtr &msg) const
 {
-  fsd_common_msgs::ControlCommandPtr cmd(new fsd_common_msgs::ControlCommand);
+  fsd_common_msgs::ControlCommand cmd_msg;
 
-  cmd->header.stamp = ros::Time::now();
-  cmd->steering_angle.data = msg->steering_angle;
-  cmd->throttle.data = msg->speed / 100.f;
+  cmd_msg.header.stamp = msg->stamp;
+  cmd_msg.steering_angle.data = msg->steering_angle;
+  cmd_msg.throttle.data = msg->speed / 100.f;
 
-  cmd_pub_.publish(cmd);
+  cmd_pub_.publish(cmd_msg);
 }
 void PathTrackingSimInterface::trajectoryCallback(const geometry_msgs::PolygonStamped::ConstPtr &msg) const
 {
-  sgtdv_msgs::Point2DArrPtr trajectory_msg(new sgtdv_msgs::Point2DArr);
-  trajectory_msg->points.reserve(msg->polygon.points.size());
+  sgtdv_msgs::Point2DArr trajectory_msg;
+  trajectory_msg.points.reserve(msg->polygon.points.size());
 
+  sgtdv_msgs::Point2D point;
   for(auto msg_point : msg->polygon.points)
   {
-    sgtdv_msgs::Point2D point;
-    //point.header = msg->header;
     point.x = msg_point.x;
     point.y = msg_point.y;
 
-    trajectory_msg->points.push_back(point);
+    trajectory_msg.points.push_back(point);
   }
 
   trajectory_pub_.publish(trajectory_msg);
 }
 void PathTrackingSimInterface::poseCallback(const fsd_common_msgs::CarState::ConstPtr &msg) const
 {
-  sgtdv_msgs::CarPosePtr car_pose_msg(new sgtdv_msgs::CarPose);
+  sgtdv_msgs::CarPose car_pose_msg;
 
-  //carPose->position.header = msg->header;
-  car_pose_msg->position.x = msg->car_state.x;
-  car_pose_msg->position.y = msg->car_state.y;
-  car_pose_msg->yaw = msg->car_state.theta;
+  car_pose_msg.position.x = msg->car_state.x;
+  car_pose_msg.position.y = msg->car_state.y;
+  car_pose_msg.yaw = msg->car_state.theta;
 
   pose_pub_.publish(car_pose_msg);
 }
 
 void PathTrackingSimInterface::velocityCallback(const fsd_common_msgs::CarStateDt::ConstPtr &msg) const
 {
-  sgtdv_msgs::CarVelPtr car_vel_msg(new sgtdv_msgs::CarVel);
-
-  car_vel_msg->speed = msg->car_state_dt.x;
-  car_vel_msg->yaw_rate = msg->car_state_dt.theta;
+  sgtdv_msgs::CarVel car_vel_msg;
+  
+  car_vel_msg.speed = msg->car_state_dt.x;
+  car_vel_msg.yaw_rate = msg->car_state_dt.theta;
 
   velocity_pub_.publish(car_vel_msg);
 }
